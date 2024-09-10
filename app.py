6d93b85f7960dde7058c80c1d8c086c989b35cae
NikolasSucadorni <nikolassucadorni@gmail.com>
2024-09-10 11:32:31 -0300

    fullcode

--- app.py
+++ app.py
@@ -3,3 +3,18 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("pizzas.csv")
+
+modelo = LinearRegression()
+x = df[["diametro"]]
+y = df[["preco"]]
+
+modelo.fit(x, y)
+
+st.title("Valor da sua Pizza")
+st.divider()
+
+diametro = st.number_input("Digite o tamanho da sua pizza")
+
+if diametro: 
+    preco_previsto = modelo.predict([[diammetro]])[0][0]
+    st.write(f"O valor da pizza com diâmetro de {diametro:.2f} é de R${preco_previsto:.2f}")
